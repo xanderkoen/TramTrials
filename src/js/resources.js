41584578e4215599c3650b987e73d45576afab6a
Xander <xkoen@hotmail.com>
2023-06-15 13:41:01 +0200

    add resources

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,17 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
+import Achtergrond from '../images/Achtergrond.jpg'
+import Grass from '../images/Grass.jpg'
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Fish: new ImageSource(fishImage),
+    Achtergrond: new ImageSource(Achtergrond),
+    Grass: new ImageSource(Grass),
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([
+    Resources.Fish,
+    Resources.Achtergrond,
+    Resources.Grass,
+])
 
 export { Resources, ResourceLoader }
