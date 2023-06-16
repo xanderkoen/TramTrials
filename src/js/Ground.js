6ec385d7683ea34e3ef11d79cc6c236373903178
Mikael <mjpeijer@gmail.com>
2023-06-16 16:06:07 +0200

    platform toegevoegd

---
+++ src/js/Ground.js
@@ -0,0 +1,16 @@
+import { CollisionType, Actor, Vector } from "excalibur"
+import { Resources } from './resources.js'
+
+export class Ground extends Actor {
+
+    constructor(x, y) {
+        super({ width: Resources.Ground.width, height: Resources.Ground.height }) // collision box!
+        this.pos = new Vector(x,y)
+    }
+
+    onInitialize(engine) {
+        this.graphics.use(Resources.Ground.toSprite())
+
+        this.body.collisionType = CollisionType.Fixed
+    }
+}
