62487bd3f71fb54d32400f20043143889992b7b1
Mikael <mjpeijer@gmail.com>
2023-06-16 14:06:45 +0200

    create player.js

--- src/js/game.js
+++ src/js/game.js
@@ -1,21 +1,57 @@
 import '../css/style.css'
-import { Actor, Engine, Vector } from "excalibur"
+import { Actor, Engine, Vector, Physics, Axis, Color, Font, FontUnit, Label, ParallaxComponent, Scene, vec} from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
+import { Player } from "../js/player.js";
 
 export class Game extends Engine {
 
-    constructor() {
-        super({ width: 800, height: 600 })
+    player
+
+    constructor() {super({
+        width: 800,
+        height: 600,
+        displayMode: 'FitScreenAndFill'
+    })
+
         this.start(ResourceLoader).then(() => this.startGame())
+    }
+    onInitialize(_engine) {
+        super.onInitialize(_engine);
+
+        // this.levelint = 1
+
+        console.log("first level")
+
+        // this.background = new Background()
+        // this.background.addComponent(new ParallaxComponent(new Vector(0.5, 0.5)))
+        // this.add(this.background)
+
+        Physics.acc = new Vector(0, 800)
+        this.player = new Player()
+        this.resetPlayer()
+        this.add(this.player)
+
+
     }
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+        // const fish = new Actor()
+        // fish.graphics.use(Resources.Fish.toSprite())
+        // fish.pos = new Vector(400, 300)
+        // fish.vel = new Vector(-10,0)
+        // this.add(fish)
+
+
+        console.log("test")
+    }
+    resetPlayer() {
+        // reset velocity
+        // this.player.vel.y = 0
+        // this.player.vel.x = 0
+        //
+        // //put back to spawn
+        // this.player.pos = this.startpos
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -2,16 +2,19 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
 import Achtergrond from '../images/Achtergrond.jpg'
 import Grass from '../images/Grass.jpg'
+import Player from '../images/fish.png'
 
 const Resources = {
     Fish: new ImageSource(fishImage),
+    Player: new ImageSource(fishImage),
     Achtergrond: new ImageSource(Achtergrond),
     Grass: new ImageSource(Grass),
 }
-const ResourceLoader = new Loader([
-    Resources.Fish,
-    Resources.Achtergrond,
-    Resources.Grass,
-])
+const resourceArray = []
+for (const key in Resources) {
+    resourceArray.push(Resources[key])
+}
 
+const ResourceLoader = new Loader(resourceArray)
+ResourceLoader.suppressPlayButton = true
 export { Resources, ResourceLoader }
