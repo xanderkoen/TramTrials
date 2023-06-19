cc21f802b99bf67fb3635e601c11ca02f3ca0f55
Mikael <mjpeijer@gmail.com>
2023-06-19 15:56:21 +0200

    eindscherm aangemaakt

---
+++ src/scenes/eindscherm.js
@@ -0,0 +1,28 @@
+import {Actor, Input, Scene, Vector,} from "excalibur";
+import {Resources} from "../js/resources";
+
+
+export class Eindscherm extends Scene {
+    game;
+    constructor() {
+        super({displayMode: 'FitScreenAndFill'});
+    }
+
+    onInitialize(engine) {
+        this.game = engine;
+
+        const background = new Actor();
+        background.graphics.use(Resources.Grass.toSprite());
+        background.pos = new Vector(0, 0);
+        background.anchor = new Vector(0, 0);
+        this.add(background);
+    }
+
+    onPreUpdate(engine) {
+        if (engine.input.keyboard.wasPressed(Input.Keys.Enter)) {
+            this.game.goToScene('Beginscherm')
+        }
+    }
+
+
+}
